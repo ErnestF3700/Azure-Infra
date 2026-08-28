64c0d73711740be7343d930d49c250b42d5525e0
ErnestoCarillo <ernestfurtak@onet.pl>
2026-08-28 10:52:38 +0200

    module/vnetpeering

--- modules/vnet_peering/outputs.tf
+++ modules/vnet_peering/outputs.tf
@@ -8,11 +8,6 @@ output "name" {
   value       = azurerm_virtual_network_peering.this.name
 }
 
-output "state" {
-  description = "Current state of the Virtual Network Peering."
-  value       = azurerm_virtual_network_peering.this.peering_state
-}
-
 output "remote_virtual_network_id" {
   description = "Resource ID of the remote Virtual Network."
   value       = azurerm_virtual_network_peering.this.remote_virtual_network_id
